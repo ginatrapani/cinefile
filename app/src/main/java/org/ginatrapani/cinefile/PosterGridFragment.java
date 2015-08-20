47a5daf64e5323e099167ebc8dbefe86cdca4fae
Gina Trapani <ginatrapani@gmail.com>
2015-08-20 17:16:02 -0400

    Add FetchMoviesTask

--- app/src/main/java/org/ginatrapani/cinefile/PosterGridFragment.java
+++ app/src/main/java/org/ginatrapani/cinefile/PosterGridFragment.java
@@ -2,16 +2,30 @@ package org.ginatrapani.cinefile;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 /**
@@ -27,6 +41,8 @@ public class PosterGridFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    public static ArrayAdapter<String> mMoviesAdapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -68,8 +84,6 @@ public class PosterGridFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_poster_grid, container, false);
 
         View rootView = inflater.inflate(R.layout.fragment_poster_grid, container, false);
 
@@ -115,6 +129,125 @@ public class PosterGridFragment extends Fragment {
         public void onFragmentInteraction(Uri uri);
     }
 
+    public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
+
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+            int numMovies = 10;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String FORECAST_BASE_URL =
+                        "http://api.themoviedb.org/3/discover/movie?";
+                final String SORT_BY_PARAM = "sort_by";
+                final String API_KEY_PARAM = "api_key";
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(SORT_BY_PARAM, params[0])
+                        .appendQueryParameter(API_KEY_PARAM, params[2])
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "Movie JSON" + moviesJsonStr);
+                try {
+                    return getMovieDataFromJson(moviesJsonStr, numMovies);
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, "Error ", e);
+                }
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+
+        /**
+         * Take the String representing the complete list of movies in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         *
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private String[] getMovieDataFromJson(String moviesJsonStr, int numDays)
+            throws JSONException {
+            final String TMDB_RESULTS = "results";
+            JSONObject moviesJson = new JSONObject(moviesJsonStr);
+            JSONArray moviesArray = moviesJson.getJSONArray(TMDB_RESULTS);
+            String[] resultStrs = new String[numDays];
+            for(int i = 0; i < moviesArray.length(); i++) {
+                // Get the JSON object representing a movie
+                JSONObject singleMovie = moviesArray.getJSONObject(i);
+                String posterPath = singleMovie.getString("poster_path");
+                resultStrs[i] = posterPath;
+            }
+            return resultStrs;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            if (result != null) {
+                mMoviesAdapter.clear();
+                for(String dayForecastStr : result) {
+                    mMoviesAdapter.add(dayForecastStr);
+                }
+            }
+        }
+    }
 }
 
 class ImageAdapter extends BaseAdapter {
