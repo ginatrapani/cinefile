b98eb94f434df531799d6fd670b24bf6cf3644e6
Gina Trapani <ginatrapani@gmail.com>
2016-02-24 13:00:24 -0500

    Implement Cursor Loader

--- app/src/main/java/org/ginatrapani/cinefile/PosterGridFragment.java
+++ app/src/main/java/org/ginatrapani/cinefile/PosterGridFragment.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,7 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-
+import android.support.v4.content.CursorLoader;
 import org.ginatrapani.cinefile.data.FetchMoviesTask;
 import org.ginatrapani.cinefile.data.ImageAdapter;
 import org.ginatrapani.cinefile.data.Movie;
@@ -21,11 +23,13 @@ import org.ginatrapani.cinefile.data.MovieContract;
 
 import java.util.ArrayList;
 
-public class PosterGridFragment extends Fragment {
+public class PosterGridFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public static ImageAdapter mMoviesAdapter;
 
     private static final String KEY_MOVIE_LIST = "movies";
+
+    private static final int MOVIES_LOADER = 0;
 
     private ArrayList<Movie> mMovies;
 
@@ -52,29 +56,9 @@ public class PosterGridFragment extends Fragment {
             mMovies = savedInstanceState.getParcelableArrayList(KEY_MOVIE_LIST);
         }
 
-        // Sort order:  vote_average.desc, popularity.desc, or favorites.
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        String sortOrder = prefs.getString(getActivity().getString(R.string.pref_key_sort),
-                getActivity().getString(R.string.pref_default_sort));
-
-        if (sortOrder.equals("vote_average.desc")) {
-            sortOrder = MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE;
-        } else if (sortOrder.equals("popularity.desc")) {
-            sortOrder = MovieContract.MovieEntry.COLUMN_POPULARITY;
-        } else if (sortOrder.equals("favorites")) {
-            // @TODO Do the join on the future favorites table
-            sortOrder = MovieContract.MovieEntry.COLUMN_POPULARITY; //
-        }
-        sortOrder += " DESC";
-        Uri moviesUri = MovieContract.MovieEntry.CONTENT_URI;
-
-        // Students: Uncomment the next lines to display what what you stored in the bulkInsert
-        Cursor cur = getActivity().getContentResolver().query(moviesUri, null, null, null,
-                sortOrder);
-
         View rootView = inflater.inflate(R.layout.fragment_poster_grid, container, false);
         GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
-        mMoviesAdapter = new ImageAdapter(this.getActivity(), cur, 0);
+        mMoviesAdapter = new ImageAdapter(this.getActivity(), null, 0);
         gridView.setAdapter(mMoviesAdapter);
 
         return rootView;
@@ -89,6 +73,12 @@ public class PosterGridFragment extends Fragment {
                     getString(R.string.pref_default_sort));
             new FetchMoviesTask(getActivity()).execute(sortOrder);
 //        }
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(MOVIES_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
     }
 
     @Override
@@ -112,5 +102,39 @@ public class PosterGridFragment extends Fragment {
     public void onSaveInstanceState(Bundle outState) {
         //outState.putParcelableArrayList(KEY_MOVIE_LIST, mMoviesAdapter.getMovies());
         super.onSaveInstanceState(outState);
+    }
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Sort order:  vote_average.desc, popularity.desc, or favorites.
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sortOrder = prefs.getString(getActivity().getString(R.string.pref_key_sort),
+                getActivity().getString(R.string.pref_default_sort));
+
+        if (sortOrder.equals("vote_average.desc")) {
+            sortOrder = MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE;
+        } else if (sortOrder.equals("popularity.desc")) {
+            sortOrder = MovieContract.MovieEntry.COLUMN_POPULARITY;
+        } else if (sortOrder.equals("favorites")) {
+            // @TODO Do the join on the future favorites table
+            sortOrder = MovieContract.MovieEntry.COLUMN_POPULARITY; //
+        }
+        sortOrder += " DESC";
+        Uri moviesUri = MovieContract.MovieEntry.CONTENT_URI;
+
+        return new CursorLoader(getActivity(),
+                moviesUri,
+                null,
+                null,
+                null,
+                sortOrder);
+    }
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        mMoviesAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mMoviesAdapter.swapCursor(null);
     }
 }
